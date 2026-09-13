ea9123f820d0c5b8da53dad67afada6027039245
GiovanaVeronezi <giovanamveronezi@outlook.com>
2026-09-13 12:10:30 -0300

    Adicionando foyo do projeto

--- src/components/Projetos/index.js
+++ src/components/Projetos/index.js
@@ -1,5 +1,5 @@
 import "./estilo.css";
-import projeto1 from "../../Imagens/projeto-1.png";
+import projeto1 from "imagens/projeto-1.png";
 import projeto2 from "../../Imagens/projeto-2.png";
 import projeto3 from "../../Imagens/projeto-3.png";
 
